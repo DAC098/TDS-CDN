72404e1de42ccd872ec5e1c171b8e3d165923049
DAC098 <dac098Git@gmail.com>
2016-08-12 21:08:14 -0700

    router seperation
    seperated the routes for the cdn and the interface to allow for less confusion comming from the same router, added modules to start making the interface and use sockets along with code building and unit testing

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const http = require('http');
 
-const router = require('./lib/router.js');
+const router = require('./lib/routers/router.js');
+
+const cdn_router = require('./lib/routers/cdn.js');
+const fs_router = require('./lib/routers/file_sys.js');
 
 var app = express();
 
-app.use('/',router);
+app.use('/',fs_router);
+
+app.use('/cdn',cdn_router);
 
 const server = http.createServer(app).listen(3050,() => {
     console.log('server listening on port 3050');
