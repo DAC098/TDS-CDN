585b905ca4b1cd3202c42e91741742b9706f506f
DAC098 <dac098Git@gmail.com>
2016-10-05 23:42:09 -0700

    updated client socket
    set the client socket to only set when it is on the client, the socket will connect to the server with the address given by the window object

--- webpack/socket.js
+++ webpack/socket.js
@@ -1,35 +1,38 @@
-var regex = /(http?s\:\/\/.*)\/.*/g;
-var result = regex.exec(window.location);
-var address = result[1];
-console.log('result from regex:',address);
+var io = require('socket.io-client');
+var log = require('./CLogs.js').makeLogger('c-socket');
 
-var socket = require('socket.io-client')('http://127.0.0.1:3050');
-var log = require('./CLogs.js').makeLogger('socket');
+var is_client = typeof window !== 'undefined';
 
-let reconnecting = false;
+if(is_client) {
 
-socket.on('connect',() => {
-    log('connected to server')
-});
+    var socket = io(window.location.origin);
 
-socket.on('error',(err) => {
-    log('error in the connection,',err);
-});
+    let reconnecting = false;
 
-socket.on('disconnect',() => log('disconnected from server'));
+    socket.on('connect',() => {
+        log('connected to server')
+    });
 
-socket.on('reconnect',() => {
-    reconnecting = false;
-    log('reconnected with server');
-});
+    socket.on('error',(err) => {
+        log('error in the connection,',err);
+    });
 
-socket.on('reconnecting',() => {
-    if(!reconnecting) {
-        log('attempting to reconnect');
-        reconnecting = true;
-    }
-});
+    socket.on('disconnect',() => log('disconnected from server'));
 
-socket.on('reconnect_failed',() => log('failed to reconnect with server'));
+    socket.on('reconnect',() => {
+        reconnecting = false;
+        log('reconnected with server');
+    });
 
-module.exports = socket;
+    socket.on('reconnecting',() => {
+        if(!reconnecting) {
+            log('attempting to reconnect');
+            reconnecting = true;
+        }
+    });
+
+    socket.on('reconnect_failed',() => log('failed to reconnect with server'));
+
+    module.exports = socket;
+
+}
